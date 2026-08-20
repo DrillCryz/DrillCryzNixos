7888364f00941a04cb7d0077aae6c28e76041fb6
DrillCryz <imsilvercryz@protonmail.com>
2026-08-20 17:49:10 -0400

    cambio 2.6

--- configuration.nix
+++ configuration.nix
@@ -1,27 +1,20 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 
 {
   imports =
-    [ # Include the results of the hardware scan.
+    [
       ./hardware-configuration.nix
       ./extra-config.nix
     ];
 
-  # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  # Use latest kernel.
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+    boot.kernelPackages = pkgs.linuxPackages_latest;
 
   networking.hostName = "DrillCryz"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
-  zramSwap = {
+    zramSwap = {
     enable = true;
     memoryPercent = 100;
   };
@@ -30,11 +23,6 @@
   { device = "/swapfile"; }
 ];
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
   networking.networkmanager.enable = true;
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
@@ -46,10 +34,8 @@
 
   hardware.opentabletdriver.enable = true;
 
-  # Set your time zone.
   time.timeZone = "America/Santiago";
 
-  # Select internationalisation properties.
   i18n.defaultLocale = "es_US.UTF-8";
 
   i18n.extraLocaleSettings = {
@@ -64,12 +50,9 @@
     LC_TIME = "es_CL.UTF-8";
   };
 
-  # Enable the X11 windowing system.
-  # You can disable this if you're only using the Wayland session.
-  services.xserver.enable = true;
+   services.xserver.enable = true;
 
-  # Enable the login manager
-  services.greetd = {
+   services.greetd = {
     enable = true;
     settings = {
       default_session = {
@@ -79,25 +62,15 @@
     };
   };
 
-  # Configure keymap in X11
   services.xserver.xkb = {
     layout = "latam";
     variant = "";
   };
 
-  # Configure console keymap
   console.keyMap = "la-latin1";
 
-  # Enable CUPS to print documents.
   services.printing.enable = true;
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users."hax" = {
     isNormalUser = true;
     description = "DrillCryz";
@@ -106,14 +79,11 @@
     packages = with pkgs; [
       kdePackages.kate
       tree
-    #  thunderbird
     ];
   };
 
-  # Install firefox.
   programs.firefox.enable = true;
 
-  # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
   environment.sessionVariables = {
@@ -127,8 +97,6 @@
     CLUTTER_BACKEND = "wayland";
   };
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
     (wine.override { wineBuild = "wine64"; })
     vesktop
@@ -157,20 +125,9 @@
     corefonts
 ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
     programs.niri.enable = true;
     programs.fish.enable = true;
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
 
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
     services.flatpak.enable = true;
 
     services.upower.enable = true;
@@ -217,18 +174,6 @@
     extraPackages32 = with pkgs.pkgsi686Linux; [ mesa ];
 };
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "26.05"; # Did you read the comment?
+  system.stateVersion = "26.05";
 
 }
